667f5bc77ceff12be13e5181303fe07ec03d13bd
Dafnik <git@dafnik.me>
2025-12-05 18:00:35 +0100

    feat: add playground & qrcode

--- projects/dfx-qrcode/src/lib/dfx-qrcode.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-dfx-qrcode',
-  imports: [],
-  template: ` <p>dfx-qrcode works!</p> `,
-  styles: ``,
-})
-export class DfxQrcode {}
--- projects/dfx-qrcode/src/public-api.ts
+++ projects/dfx-qrcode/src/public-api.ts
@@ -2,4 +2,10 @@
  * Public API Surface of dfx-qrcode
  */
 
-export * from './lib/dfx-qrcode';
+export * from './lib/download.helper';
+export * from './lib/qrcode.component';
+export * from './lib/qrcode.config';
+export * from './lib/qrcode.feature';
+export * from './lib/qrcode.provider';
+export * from './lib/qrcode-image.feature';
+export * from './lib/types';
