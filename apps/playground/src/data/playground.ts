3bd23469c98e5ce6eb186415c5fa29dd140d1fe0
Dafnik <git@dafnik.me>
2026-04-08 20:11:25 +0200

    feat: redesign website

--- apps/playground/src/data/playground.ts
+++ apps/playground/src/data/playground.ts
@@ -4,11 +4,6 @@ export type PlaygroundProject = {
   href: string;
 };
 
-export type PlaygroundVariant = {
-  label: string;
-  href: string;
-};
-
 export const projects: PlaygroundProject[] = [
   {
     title: 'dfx-bootstrap-table',
@@ -35,13 +30,4 @@ export const projects: PlaygroundProject[] = [
     text: 'Angular services and directives for frontend authorization based on @open-policy-agent/opa',
     href: 'opa',
   },
-];
-
-export const variants: PlaygroundVariant[] = [
-  { label: 'Original', href: '/' },
-  { label: '1', href: '/1/' },
-  { label: '2', href: '/2/' },
-  { label: '3', href: '/3/' },
-  { label: '4', href: '/4/' },
-  { label: '5', href: '/5/' },
 ];
