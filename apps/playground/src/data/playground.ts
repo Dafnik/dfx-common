fc0a08c8110892881233fbcbb708254cfd21633b
Dafnik <git@dafnik.me>
2026-04-09 16:17:05 +0200

    feat: redesign website (#117)

---
+++ apps/playground/src/data/playground.ts
@@ -0,0 +1,33 @@
+export type PlaygroundProject = {
+  title: string;
+  text: string;
+  href: string;
+};
+
+export const projects: PlaygroundProject[] = [
+  {
+    title: 'dfx-bootstrap-table',
+    text: 'Angular table CDK implementation for Bootstrap with filtering, sorting and pagination.',
+    href: 'bootstrap-table',
+  },
+  {
+    title: 'dfx-qrcode',
+    text: 'Tiny and simple-to-use Angular QR-Code generator library.',
+    href: 'qrcode',
+  },
+  {
+    title: 'dfx-transloco-markup',
+    text: 'A translation component to render translations with markup',
+    href: 'transloco-markup',
+  },
+  {
+    title: 'dfx-theme',
+    text: 'Tiny and simple-to-use Angular Theme management library.',
+    href: 'theme',
+  },
+  {
+    title: 'dfx-opa',
+    text: 'Angular services and directives for frontend authorization based on @open-policy-agent/opa',
+    href: 'opa',
+  },
+];
